8a27cb51bb3d4dccc298de68803385b2fb5b067a
Daniel Takev <daniel.takev@progress.com>
2025-03-12 21:50:27 +0200

    Added new file

--- Java_Basic/src/Main2.java
+++ Java_Basic/src/Main2.java
@@ -1,8 +1,8 @@
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("New Version Change");
         // New comment 222
-
+        // NEW
     }
 }
